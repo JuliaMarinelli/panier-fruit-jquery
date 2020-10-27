3cfd2d8fbc1db4cca59a06f35c3db64cc4b7bfba
Julia Marinelli <julia_marinelli@hotmail.fr>
2020-10-27 09:38:24 +0100

    add main.js

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const fruits = $("#liste_fruits")
+const panier = $("#panier")
+const addButton = $("#ajouter")
+const delButton = $("#supprimer")
+
+let fruit;
+let fruitToDel;
+
+$(window).on('load', function(){
+    fruit = fruits.val();
+    addButton.attr("disabled", fruit === null);
+
+    fruitToDel = panier.val();
+    delButton.attr("disabled", fruitToDel === null);
+})
+
+fruits.change(function() {
+    fruit = fruits.val();
+    
+    addButton.attr("disabled", fruit === null);
+})
+
+panier.change(function() {
+    fruitToDel = panier.val();
+    delButton.attr("disabled", fruitToDel === null);
+})
+
+addButton.click(function(){
+    if(fruit !== null){
+        panier.append($("<option></option>").attr("value", fruit).text(fruit));
+        fruits.children('[value="'+ fruit + '"]').remove()
+        fruit = null;
+    }
+})
+
+
+delButton.click(function(){
+    if(fruitToDel !== null){
+        fruits.append($("<option></option>").attr("value", fruitToDel).text(fruitToDel));
+        panier.children('[value="'+ fruitToDel + '"]').remove()
+        fruitToDel = null;
+    }
+})
